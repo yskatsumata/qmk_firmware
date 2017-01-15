f3607f3c729e0df8ec454a9c5d993750574fb1c8
yskatsumata <yskatsumata@gmail.com>
2017-01-15 14:34:21 +0900

    MHEN key

--- keyboards/ergodox/keymaps/yskatsumata/keymap.c
+++ keyboards/ergodox/keymaps/yskatsumata/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         KC_ESC,         KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        LT(SYMB,KC_GRV),KC_LGUI,     LALT(KC_LSFT),   KC_LALT,KC_RGHT,
+        LT(SYMB,KC_GRV),KC_LGUI,     LALT(KC_LSFT),   KC_LALT,CTL_T(KC_MHEN),
                                               ALT_T(KC_APP),  KC_LGUI,
                                                               KC_HOME,
                                                KC_SPC,KC_BSPC,KC_END,
